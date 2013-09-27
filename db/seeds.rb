b91299532fe8fc97815415b8247f68e6d5502d1e
Dan Tao <daniel.tao@gmail.com>
2013-09-26 17:47:40 -0700

    updated seeds.rb w/ tests for chunk function

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,6 +52,28 @@ function chunk(array, chunkSize) {
 }
 JAVASCRIPT
 
+add_tests chunk, <<-JAVASCRIPT
+describe('chunk', function() {
+  it('has no effect on an empty array', function() {
+    expect(chunk([], 3)).toEqual([]);
+  });
+
+  it('does not modify the array in-place', function() {
+    var array = [1, 2, 3];
+    chunk(array, 2);
+    expect(array).toEqual([1, 2, 3]);
+  });
+
+  it('splits an array into chunks of the specified size', function() {
+    expect(chunk([1, 2, 3, 4], 2)).toEqual([[1, 2], [3, 4]]);
+  });
+
+  it('includes a partial final chunk, if necessary', function() {
+    expect(chunk([1, 2, 3], 2)).toEqual([[1, 2], [3]]);
+  });
+});
+JAVASCRIPT
+
 add_implementation dan, chunk, <<-JAVASCRIPT
 function chunk(array, chunkSize) {
   var chunks = [],
